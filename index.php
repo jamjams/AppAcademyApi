4093e27481f195749adcf6fcc1fb43ce7ca36b84
Jahmelah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-05-11 12:13:33 -0700

    milestone 4

--- index.php
+++ index.php
@@ -6,8 +6,8 @@ session_start();
 
 /*Make constants using define*/
 /*we can call the name anywhere else in the file because we gave it a value*/
-define('client_ID', '871b92afb12e4290b6162a24bceeb547');
-define('client_Secret', '3a0169d5e8b94ba799bdb8933c97c4e7');
+define('clientID', '871b92afb12e4290b6162a24bceeb547');
+define('clientSecret', '3a0169d5e8b94ba799bdb8933c97c4e7');
 define('redirectURI', 'http://localhost/AppAcademyApi/index.php');
 define('ImageDirectory', 'pics/');
 
@@ -22,7 +22,7 @@ define('ImageDirectory', 'pics/');
 <body>
 	<!-- Creating a login for people to go and get approval for our web app to access their Instagram account 
 	After getting approval we are now going to have the information so we can play with it -->
-	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo client_ID; ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN</a>
+	<a href="https:api.instagram/oauth/authorize/?client_id=<?php echo clientID; ?>&redirect_uri=<?php echo redirectURI ?>&response_type=code">LOGIN</a>
 
 </body>
 </html>
