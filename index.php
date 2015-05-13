151bf47806c127314a191ad8725a446fca004854
Jahmelah Maat-Hotep <jahmelah.maathotep@gmail.com>
2015-05-13 11:30:35 -0700

    finished half of milestone 10

--- index.php
+++ index.php
@@ -27,6 +27,14 @@ function connectToInstagram($url){
 	return $result;
 
 }
+/*Function to get userID cause userName doesn't allow us to get pictures!*/
+function getUserID($userName){
+	$url = 'http://api.instagram.com/v1/users/search?q='.$userName.'&client_id='.clientID;
+	$instagramInfo = connectToInstagram($url);
+	results = json_decode($instagramInfo, true);
+
+	echo $results['data']['0']['id'];
+}
 /*checks for a booliean, if it is true or not true*/
 if (isset($_GET['code'])){
 	$code = ($_GET['code']);
@@ -49,7 +57,7 @@ $result=curl_exec($curl);
 curl_close($curl);
 
 $results = json_decode($result,true);
-echo $results['user']['username'];
+getUserID($results['user']['username']);
 }
 else{
 ?>
